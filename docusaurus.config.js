ceac1edba699bbe9ad347cf9bc21499234b8b1a6
Digi <digiworm0@gmail.com>
2026-02-05 00:07:31 -0600

    Added Control Loops Documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,21 +1,19 @@
 // @ts-check
 
 import {themes as prismThemes} from 'prism-react-renderer';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'DevilLib',
     tagline: 'Building bots, raising hell',
     favicon: 'img/favicon.ico',
+    future: {v4: true},
 
-    future: {
-        v4: true,
-    },
-
+    // Deployment Config
     url: 'https://docs.devilbots.org/',
     baseUrl: '/',
-
-    // GitHub pages deployment config.
     organizationName: 'UWST-Robotics',
     projectName: 'DevilBots-Docs',
 
@@ -33,6 +31,10 @@ const config = {
                 docs: {
                     sidebarPath: './sidebars.js',
                     editUrl: 'https://github.com/UWST-Robotics/DevilLib-Docs/',
+
+                    // Applied KaTEX Plugin
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
                 },
                 theme: {
                     customCss: './src/css/custom.css',
@@ -41,10 +43,20 @@ const config = {
         ],
     ],
 
+    stylesheets: [
+        // Applies KaTEX Stylesheet
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity:
+                'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+        },
+    ],
+
     themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            // Replace with your project's social card
             image: 'img/og-image.png',
             colorMode: {
                 respectPrefersColorScheme: true,
@@ -52,7 +64,7 @@ const config = {
             navbar: {
                 title: 'DevilLib',
                 logo: {
-                    alt: 'My Site Logo',
+                    alt: 'DevilLib Logo',
                     src: 'img/logo.png',
                 },
                 items: [
